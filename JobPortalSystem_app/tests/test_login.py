c4048ca89f56ad71f19baa6bea48b5bf25ef1199
Luong Nguyen Khoi <khoinguyenluong012@gmail.com>
2025-07-13 10:52:05 +0700

    test

--- JobPortalSystem_app/tests/test_login.py
+++ JobPortalSystem_app/tests/test_login.py
@@ -3,10 +3,10 @@ from dao import check_user
 
 class TestAuth(unittest.TestCase):
     def test_correct_credentials(self):
-        self.assertTrue(check_user(username="admin", password="123"))
+        self.assertTrue(check_user(username="admin", password=123))
 
     def test_wrong_username(self):
-        self.assertFalse(check_user(username="wrong", password="123"))
+        self.assertFalse(check_user(username="wrong", password=123))
 
     def test_wrong_password(self):
-        self.assertFalse(check_user(username="admin", password="wrong"))
+        self.assertFalse(check_user(username="admin", password=456))
