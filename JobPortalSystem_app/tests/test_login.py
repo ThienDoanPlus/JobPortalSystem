8f6edf21d94064b4c6ccaebc8b93c9da786256ea
Luong Nguyen Khoi <khoinguyenluong012@gmail.com>
2025-07-13 10:45:25 +0700

    test Jenkins

--- JobPortalSystem_app/tests/test_login.py
+++ JobPortalSystem_app/tests/test_login.py
@@ -1,16 +1,12 @@
-# tests/test_login.py
 import unittest
 from dao import check_user
 
-class TestLogin(unittest.TestCase):
+class TestAuth(unittest.TestCase):
     def test_correct_credentials(self):
-        self.assertTrue(check_user("admin", "123"))
+        self.assertTrue(check_user(username="admin", password="123"))
+
+    def test_wrong_username(self):
+        self.assertFalse(check_user(username="wrong", password="123"))
 
     def test_wrong_password(self):
-        self.assertFalse(check_user("admin", "wrongpass"))
-
-    def test_nonexistent_user(self):
-        self.assertFalse(check_user("notexist", "123"))
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertFalse(check_user(username="admin", password="wrong"))
