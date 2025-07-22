53d1cd5c32f18f099337023d458ee30c612b42c4
Thien Doan <nguyenthiendoan2@gmail.com>
2025-07-22 17:57:35 +0700

    frontend v1

--- JobPortalSystem_app/index.py
+++ JobPortalSystem_app/index.py
@@ -1,17 +1,16 @@
-
-from sqlalchemy import text
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-@app.route('/JobPortalSystem')
-@app.route('/')
-def index():
-    return "<h1>Welcome to Job Portal System!!!</h1>"
 
+# Config database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Admin%40123@localhost/jobportal'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+@app.route("/")
+def index():
+    return "<h1>Welcome to Job Portal System</h1>"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
